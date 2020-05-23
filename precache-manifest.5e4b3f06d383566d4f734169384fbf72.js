73f5b9fab3597c20bcc04a2f33fd298ecad2a384
Evelyn Pinjen Wu <evelynpwu@berkeley.edu>
2020-05-22 19:04:32 -0700

    Updates

---
+++ precache-manifest.5e4b3f06d383566d4f734169384fbf72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0309b8421c790f7c5013986217edb92",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6d6d48728a4fb7d8295",
+    "url": "/monsters-rolodex/static/css/main.9f2588f0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d6d48728a4fb7d8295",
+    "url": "/monsters-rolodex/static/js/main.952d552e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
